ee11f165153bce55d373aba693a970c34aac4dcb
Isaac Alvarez <alvarezisaac90@gmail.com>
2020-03-19 21:32:27 -0400

    webpack - devlopment server

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+
 module.exports = {
+	mode: 'development',
 	entry: {
 		app: './src/index.js',
 		print: './src/print.js',
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
 	},
 	plugins: [
 		new CleanWebpackPlugin(),
@@ -16,5 +22,6 @@ module.exports = {
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
 	},
 };
